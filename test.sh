d2e2b85eb53840c76a40a24002912e9b963eb50a
facundoy <facundoy@umich.edu>
2024-08-26 14:22:12 -0400

    added the test.sh file to run a job in lighthouse

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=example_job
+#SBATCH --mail-user=facundoy@umich.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4g
+#SBATCH --time=14:00:00
+#SBATCH --partition=alrodri-a100
+#SBATCH --output=/home/%u/%x-%j.log
+
+# The application(s) to execute along with its input arguments and options:
+
+python main.py
