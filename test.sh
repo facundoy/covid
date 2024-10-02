aa4c0069d3b930ae19fa06cb4dd6276c4f4a91a1
facundoy <facundoy@umich.edu>
2024-10-02 15:48:22 -0400

    Modified job script

--- test.sh
+++ test.sh
@@ -7,9 +7,10 @@
 #SBATCH --mail-user=facundoy@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=4g
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=47G
+#SBATCH --gpus=1
 #SBATCH --time=14:00:00
 #SBATCH --partition=alrodri-a100
 #SBATCH --output=/home/%u/%x-%j.log
