f2a78ada4b5167334b79a8ddfb824c1e4b5e3087
facundoy <facundoy@umich.edu>
2024-11-20 17:11:57 -0500

    Changed test.sh

--- test.sh
+++ test.sh
@@ -7,15 +7,15 @@
 #SBATCH --mail-user=facundoy@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=47G
-#SBATCH --gpus=1
+#SBATCH --gpus=2
 #SBATCH --time=14:00:00
 #SBATCH --partition=alrodri-a100
 #SBATCH --output=/home/%u/%x-%j.log
 
 # The application(s) to execute along with its input arguments and options:
 
+nvidia-smi
 python main.py
-python graph.py
